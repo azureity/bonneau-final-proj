a3d240e89995b42a4e4f77de549fe5b4bf2b88f1
Vince <vz309@nyu.edu>
2014-12-02 10:50:15 -0500

    renamed main3.py to buildraw.py. added bunch of comments. made binoutput script. will update readme

--- buildraw.py
+++ buildraw.py
@@ -1,29 +1,28 @@
-import sys
-import os
 import argparse
 from geopy.geocoders import Nominatim
 from itertools import izip
 import csv
-import time
 
-# Arguments here
+# Add arguments for cli use
 parser = argparse.ArgumentParser(description='Sift through data')
 parser.add_argument('-t', dest="tripdat", help="input a .csv tripdata file to sift through")
 parser.add_argument('-f', dest="faredat", help="input a .csv faredata file to sift through")
 args = parser.parse_args()
 
+# Line counter to keep track of line number in csv's
 ctr = 0
 
-# Create an external file that saves all the lines that cannot be resolved
-line_save = open('excludedlines.txt', 'w')
-
-# To resolve first pass
+# Open the tripdata and faredata files at the same time
 with open(args.tripdat) as tripdat, open(args.faredat) as faredat:
+	# Skip header lines
 	next(faredat)
 	next(tripdat)
+	# Open new csv which will hold the raw output
 	outputfile = open('rawoutput.csv', 'wb')
 	output = csv.writer(outputfile, delimiter=',', quoting=csv.QUOTE_ALL)
+	# Initialize geo resolver
 	geolocator = Nominatim()
+	# Go through each line in the fare and trip data
 	for x, y in izip(tripdat, faredat):
 		x_ori = x.strip()
 		x = x_ori.split(',')
@@ -39,14 +38,19 @@ with open(args.tripdat) as tripdat, open(args.faredat) as faredat:
 		# Try 10 times to geolocate, else throw error and give up
 		while (attempts < 10):
 			try:
+				# Gets the geolocation based on LAT/LONG
 				location_pickup = geolocator.reverse(x[-3] + ',' + x[-4])
 				location_dropoff = geolocator.reverse(x[-1] + ',' + x[-2])
-				
+				# If it works, print out the location
 				print(location_pickup.address)
 				print(location_dropoff.address)
+				# Success cass, exit loop
 				break
+			# If the geo resolution times out
 			except geopy.exc.GeocoderTimedOut:
+				# Log a failed resolution
 				print('failed to geo resolve line: ' + str(ctr) + '\t trying again until != 10 : #' + str(attempts))
+				# Incrememt attempts
 				attempts += 1
 		
 		# To show that the program has not crashed
@@ -54,16 +58,22 @@ with open(args.tripdat) as tripdat, open(args.faredat) as faredat:
 
 		# Build output line for .csv
 		output_line = x + y
-		location_line = [location_pickup.address, location_dropoff.address, location_pickup.address.split(',')[2], location_dropoff.address.split(',')[2]]
+		# Find the neighbrhodd in the list - NEIGHBORHOODS SHOULD BE IN INDEX -7
+		locsplitp = location_pickup.address.split(',')
+		locsplitd = location_dropoff.address.split(',')
 
+		# Builds all locations: pickup address, dropoff address, pickup neighborhood, dropoff neighborhood
+		location_line = [location_pickup.address, location_dropoff.address, locsplitp[-7], locsplitd[-7]]
+
+		# Build full output here
 		output_line += location_line
 
 		# Write ROW, not write ROWS
 		output.writerow(output_line)
 
-		# Test with 15 lines
+		# Test with 15 lines - COMMENT OUT OR DELETE IN PRODUCTION
 		if(ctr == 15):
 			break
 
-# Close line save file
-line_save.close()
+# Close files for cleanup
+outputfile.close()
